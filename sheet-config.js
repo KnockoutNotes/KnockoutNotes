4984f53af1483ed1fccc87110be55f7c0ca7a360
KnockoutNotes <knockoutnotes.anaesthesia@gmail.com>
2026-08-23 13:01:15 +0530

    Update sheet-config.js

--- sheet-config.js
+++ sheet-config.js
@@ -1,5 +1,2 @@
-// KNOCKOUTNOTES CONTENT SOURCE
-// Paste your Google Sheets "Publish to web" CSV URL between the quotes.
-// After this one-time setup, add/edit content only in Google Sheets.
 const KNOCKOUTNOTES_SHEET_CSV =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZyYkUytn0FaLLpAo1zBIBveI18T0XDOCMl435h9ffBm5oqYPxVggA0K2SF2WNV1vMDvgc49G9H8vf/pubhtml";
+  "https://docs.google.com/spreadsheets/d/1tRt6Q3FadXqB-mmVGMRcOWUxSnWEazN2fXmqQT69dso/gviz/tq?tqx=out:csv&gid=0";
