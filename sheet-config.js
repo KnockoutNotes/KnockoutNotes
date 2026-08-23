d3ef7fdad443825f0097a16e5b748d0c0cd0ad42
KnockoutNotes <knockoutnotes.anaesthesia@gmail.com>
2026-08-23 13:25:59 +0530

    Update sheet-config.js

--- sheet-config.js
+++ sheet-config.js
@@ -1,3 +1,3 @@
 // KnockoutNotes Google Sheets data source
 const KNOCKOUTNOTES_SHEET_CSV =
-  "https://docs.google.com/spreadsheets/d/1tRt6Q3FadXQb-mmVGMRcOWUxSnWEazN2fXmqQT69dso/gviz/tq?tqx=out:csv&gid=0";
+  "https://docs.google.com/spreadsheets/d/1tRt6Q3FadXqB-mmVGMRcOWUxSnWEazN2fXmqQT69dso/gviz/tq?tqx=out:csv&gid=0";
